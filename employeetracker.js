3d6f01af97bc79589bd128a426b0c02d128dd751
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-06-14 02:26:28 -0700

    updated for viewdepartment code

--- employeetracker.js
+++ employeetracker.js
@@ -1,45 +1,78 @@
-const mysql = require("mysql");
 const inquirer = require("inquirer");
+const Query = require("query.js");
+const query = new Query();
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  // Your port; if not 3306
-  port: 3306,
-  // Your username
-  user: "",
-  // Be sure to update with your own MySQL password!
-  password: "",
-  database: "",
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-  runSearch();
-});
-
-const runSearch = () => {
+const startApp = () => {
   inquirer
     .prompt({
       name: "action",
       type: "rawlist",
       message: "What would you like to do?",
-      choices: ["", "", "", "", ""],
+      choices: [
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "Update employee role",
+        "Update employee managers",
+        "View employees by Manager",
+        "Delete an employee",
+        "Delete a department",
+        "Delete a role",
+        "View the total utilized buget of a department",
+        "Quit",
+      ],
     })
     .then((answer) => {
       switch (answer.action) {
-        case "":
+        case "View all departments":
+          let department = query.viewdepartment();
+          console.log("\n");
+          console.log(" ** Departments **");
+          console.log("\n");
+          console.table(department);
           break;
 
-        case "":
+        case "View all roles":
           break;
 
-        case "":
+        case "View all employees":
           break;
 
-        case "":
+        case "Add a department":
           break;
 
-        case "":
+        case "Add a role":
+          break;
+
+        case "Add an employee":
+          break;
+
+        case "Update employee role":
+          break;
+
+        case "Update employee managers":
+          break;
+
+        case "Delete an employee":
+          break;
+
+        case "Delete a department":
+          break;
+
+        case "Delete a role":
+          break;
+
+        case "View the total utilized buget of a department":
+          break;
+
+        case "View employees by Manager":
+          break;
+
+        case "Quit":
+          query.quit();
           break;
 
         default:
@@ -48,3 +81,5 @@ const runSearch = () => {
       }
     });
 };
+
+startApp();
