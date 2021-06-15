7e0d5829db204e7e28b7e2f7b645e2f8ac6fc348
Gunjan Bharagava <gunjanb2006@gmail.com>
2021-06-15 14:07:18 -0700

    updated few methods for sql query

--- query.js
+++ query.js
@@ -1,18 +1,84 @@
-const connection = require("connection.js");
-
+//const connection = require("./connection");
+//var value;
 class Query {
   //METHODS
-  viewdepartment() {
-    connection.query(`SELECT * FROM department`, (err, res) => {
-      if (err) throw err;
+  // viewdepartment(connection) {
+  //   connection.query(`SELECT * FROM departments`, (err, res) => {
+  //     if (err) throw err;
+  //     value = res;
 
-      //   if (res.length > 0) {
-      return res;
-      //   }else
+  //     console.log("value", value);
+  //     console.log("res", res);
+  //     return res;
+  //   });
+  // }
+
+  viewDepartment(connection) {
+    return new Promise((resolve, reject) => {
+      connection.query(`SELECT * FROM departments`, (err, res) => {
+        if (err) {
+          return reject(err);
+        }
+        let rows = res.map((row) => row.dept_name);
+        resolve(rows);
+      });
     });
   }
 
-  quit() {
+  viewEmployees(connection) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        `SELECT CONCAT(first_name, ' ', last_name) AS fullName FROM employees`,
+        (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          let nameArray = res.map((row) => row.fullName);
+          // console.log(nameArray);
+          resolve(nameArray);
+        }
+      );
+    });
+  }
+
+  viewRoles(connection) {
+    return new Promise((resolve, reject) => {
+      connection.query(`SELECT * FROM roles`, (err, res) => {
+        if (err) {
+          reject(err);
+        }
+        let role = res.map((row) => row.title);
+        resolve(role);
+      });
+    });
+  }
+
+  addDepartment(deptName, connection) {
+    return new Promise((resolve, reject) => {
+      connection.query(
+        `INSERT INTO departments SET ?`,
+        [
+          {
+            dept_name: deptName,
+          },
+        ],
+        (err, res) => {
+          if (err) {
+            reject(err);
+          }
+          //console.log(res);
+          resolve("Department Name added Sucessfully!");
+        }
+      );
+    });
+  }
+
+  // addRole(title, salary, department_id, connection) {
+  //   return new Promise((resolve, reject) => {
+  //     connection.query();
+  //   });
+  // }
+  quit(connection) {
     connection.end();
   }
 }
